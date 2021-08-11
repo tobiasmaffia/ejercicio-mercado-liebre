8a6603afc257f65e66e7f685d2a33872a402210d
tobiasmaffia <maffiatobias@gmail.com>
2021-08-10 22:35:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3129, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
